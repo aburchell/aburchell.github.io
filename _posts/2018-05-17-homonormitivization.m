96a96f8203cf4cecb7aeae92835f3a4c1693585f
aburchell <augustin.burchell@gmail.com>
2018-05-17 15:09:24 -0400

    Create 2018-05-17-homonormitivization.m

---
+++ _posts/2018-05-17-homonormitivization.m
@@ -0,0 +1,33 @@
+---
+layout: post
+title: Homonormativization
+date: 2018-05-17
+categories: entry
+---
+## The Word
+Homonormativization
+
+## Where encountered?
+I encountered this word in a paper by my roommate, after she posted it on her Instgram story, presumably to
+highlight the almost comically long nature of the word.
+
+## Meaning Guess
+Based on the high compositionality of the word, I would guess that it means the act of normalizing homosexuality.
+
+## Meaning
+Although I could not find a definition for this word (though it has been used, and is not a new word!), I did 
+find information about *homonormativity,* which can either mean "The assumption that sexual and romantic 
+attraction and activity between people of the same sex is normal, especially in a culture which also treats 
+opposite-sex activity as normal, and which assumes all people are either male or female in both sex and gender,"
+or "The adoption of heterosexist values, beliefs and norms into the queer community."
+
+## Morphology and reflection
+While I could not find a definition of this word exactly, the productivity of the suffix *-ization*, which 
+forms nouns refering to the act of doing something, gives me a lot of information about the word. The entire
+word is very compositional and agglutinating, making it perfectly understandable despite its length and 
+apparent complexity.
+
+## Source
+`https://en.wiktionary.org/wiki/-ization`
+
+`https://en.wiktionary.org/wiki/homonormativity`
